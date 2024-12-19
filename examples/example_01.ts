209c977c5e42c730468cffded67193767ae63efc
pie111 <pranavks1234@gmail.com>
2024-12-19 16:11:13 +0530

    added single table join support

--- examples/example_01.ts
+++ examples/example_01.ts
@@ -3,5 +3,6 @@ import QueryCrafter from "../src/main.js";
 const filters = {type:"car", color:"red", year:"2019"};
 const fields = ["model", "year", "color"];
 const table = "cars";
-const queryCrafter = new QueryCrafter(filters, fields, table);
+const queryCrafter = new QueryCrafter(filters, fields, table, 1, 100);
+queryCrafter.joinConditions("colors", {cars:"color", colors:"name"});
 console.log(queryCrafter.buildFinalQuery());
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ class QueryCrafter {
   conditions: string = '';
   offset: number;
   limit: number;
+  joinQuery: string = '';
 
   constructor(filters: { [key: string]: string }, fields: Array<string>, table: string, offset: number = 0,limit:number) {
     this.filters = filters;
@@ -14,28 +15,52 @@ class QueryCrafter {
     this.limit = limit;
   }
 
-  craftConditions() {
+  private craftConditions() {
     for (const key in this.filters) {
       if (this.filters[key] !== '') {
         this.conditions += ` ${key}=${this.filters[key]} &`;
       }
     }
-    this.conditions = this.conditions.trim().replace(/&+$/, '') + ';';
+    this.conditions = this.conditions.trim().replace(/&+$/, '');
   }
 
-  addOffset(offset: number) {
-    this.conditions += ` OFFSET ${offset};`;
+  private addOffset(offset: number) {
+    this.conditions += ` OFFSET ${offset}`;
   }
 
-  addLimit(limit: number) {
-    this.conditions += ` LIMIT ${limit};`;
+  private addLimit(limit: number) {
+    this.conditions += ` LIMIT ${limit}`;
+  }
+
+  /**
+   * @param tableTobeJoined: string
+   * @param joinCondition: { [key: string]: string } eg :-  {table1 : field1,table2:field2} then the join would be table1.field1 = table2.field2
+   */
+  joinConditions(tableTobeJoined: string, joinCondition: { [key: string]: string }) {
+    this.joinQuery += ` JOIN ${tableTobeJoined} `;
+    let joinConditionQuery = '';
+    const totalKeyCount = Object.keys(joinCondition).length
+    if(totalKeyCount < 2) {
+      throw new Error('Join condition should have atleast two tables');
+    }
+    let keyCount = 0;
+    for (const key in joinCondition) {
+      keyCount ++;
+      joinConditionQuery += `${key}.${joinCondition[key]}`;
+      if(keyCount < totalKeyCount) joinConditionQuery += ' = ';
+    }
+    joinConditionQuery = joinConditionQuery.replace(/=+$/, '');
+    this.joinQuery += `ON ${joinConditionQuery}`;
   }
 
   buildFinalQuery() {
     this.craftConditions();
     if(this.offset) this.addOffset(this.offset);
     if(this.limit) this.addLimit(this.limit);
-    return `SELECT ${this.fields.join(', ')} FROM ${this.table} WHERE ${this.conditions.trim()}`;
+    let query = `SELECT ${this.fields.join(', ')} FROM ${this.table}`;
+    if(this.joinQuery) query += this.joinQuery;
+    query += ` WHERE ${this.conditions.trim()};`;
+    return query;
   }
 
 }
